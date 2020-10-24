2ece3416060e09ef08eb1c4da09685d25f448e36
Trevor Puccini <trevorpuccini21@gmail.com>
2020-10-23 21:08:07 -0700

    Add styling touch-ups to Login form

--- components/LoginForm/LoginForm.js
+++ components/LoginForm/LoginForm.js
@@ -1,27 +1,37 @@
 import styles from "./LoginForm.module.css";
+import Link from "next/link";
 
 const LoginForm = () => (
-  <form className={styles.LoginForm}>
-    <label className={styles.inputLabel} for="email">
-      Email:
-    </label>
-    <input
-      className={styles.email}
-      name="email"
-      type="text"
-      placeholder="Email address"
-    />
-    <label className={styles.inputLabel} for="password">
-      Password:
-    </label>
-    <input
-      className={styles.password}
-      name="password"
-      type="text"
-      placeholder="Password"
-    />
-    <input className={styles.submit} type="submit" />
-  </form>
+  <div className={styles.LoginForm}>
+    <h3>Login to Get Organized</h3>
+    <form className={styles.formBody}>
+      <label className={styles.inputLabel} for="email">
+        Email:
+      </label>
+      <input
+        className={styles.email}
+        name="email"
+        type="text"
+        placeholder="Email address"
+      />
+      <label className={styles.inputLabel} for="password">
+        Password:
+      </label>
+      <input
+        className={styles.password}
+        name="password"
+        type="text"
+        placeholder="Password"
+      />
+      <input className={styles.submit} type="submit" value="Login" />
+    </form>
+    <div className={styles.signUp}>
+      <p>New here?</p>
+      <Link href="/">
+        <a>Sign Up</a>
+      </Link>
+    </div>
+  </div>
 );
 
 export default LoginForm;
