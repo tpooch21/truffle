e914b96a60b4f5769121b63abbf82d3f3d42c2fc
Trevor Puccini <trevorpuccini21@gmail.com>
2020-10-23 18:27:24 -0700

    Set up initial home page configuration

---
+++ pages/boards/index.js
@@ -0,0 +1,10 @@
+import BoardsContainer from "../../containers/BoardsContainer/BoardsContainer";
+import Layout from "../../components/Layout/Layout";
+
+const BoardsPage = () => (
+  <Layout>
+    <BoardsContainer />
+  </Layout>
+);
+
+export default BoardsPage;
